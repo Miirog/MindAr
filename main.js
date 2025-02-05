6bbbaa906e5d8c02ff122ddde02bc3796232aa78
Igor Dal Col <igormdalcol@gmail.com>
2025-02-05 17:03:41 -0300

    update targets

--- main.js
+++ main.js
@@ -7,3 +7,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
 });
 
 //animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"
+{
+  /* <a-plane src="#card" position="0 0 0" height="0.552" width="1" rotation="0 0 0"></a-plane> */
+}
