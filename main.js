093a917c9cc2c18db6b740007c0c27c4b9d5084a
Igor Dal Col <igormdalcol@gmail.com>
2025-02-05 16:46:07 -0300

    update button

--- main.js
+++ main.js
@@ -2,5 +2,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const button = document.getElementById("myButton");
   button.addEventListener("click", () => {
     alert("Button was clicked!");
+    console.log("Button Click");
   });
 });
+
+//animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"
